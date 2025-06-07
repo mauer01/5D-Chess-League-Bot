ddf253ae1457410f5c85f01e165986880505e736
mauer01 <17260044+mauer01@users.noreply.github.com>
2025-06-08 00:37:57 +0200

    feat: creation string mapping to table structure for easy string building

--- constants.py
+++ constants.py
@@ -1,5 +1,8 @@
 ROLES_CONFIG_FILE = "elo_roles.csv"
 SQLITEFILE = "elo_bot.db"
+K_FACTOR = 25
+INITIAL_ELO = 1380
+
 DATABASE_STRUCTURE = {
     "pairings": [
         "id",
@@ -29,4 +32,118 @@ DATABASE_STRUCTURE = {
         "league",
     ],
     "elo_history": ["id", "player_id", "elo_change", "timestamp"],
+    "player_aliases": ["id", "player_id", "alias"],
+}
+
+DATABASE_STRUCTURE_CREATIONSTRINGMAPPING = {
+    "Tables": {
+        "players": "id INTEGER PRIMARY KEY",
+        "pending_reps": "id INTEGER PRIMARY KEY AUTOINCREMENT",
+        "seasons": "season_number INTEGER PRIMARY KEY",
+        "pairings": "id INTEGER PRIMARY KEY AUTOINCREMENT",
+        "match_history": "match INTEGER PRIMARY KEY AUTOINCREMENT",
+        "elo_history": "id INTEGER PRIMARY KEY AUTOINCREMENT",
+        "player_aliases": "id INTEGER PRIMARY KEY AUTOINCREMENT",
+    },
+    "players": {
+        "elo": "REAL DEFAULT 1380",
+        "wins": "INTEGER DEFAULT 0",
+        "losses": "INTEGER DEFAULT 0",
+        "draws": "INTEGER DEFAULT 0",
+        "signed_up": "INTEGER DEFAULT 0",
+    },
+    "pairings": {
+        "player1_id": "INTEGER",
+        "player2_id": "INTEGER",
+        "result1": "REAL DEFAULT null",
+        "result2": "REAL DEFAULT null",
+        "season_number": "INTEGER",
+        "group_name": "TEXT",
+        "foreignkeyconstraint": """
+                FOREIGN KEY
+                (
+                    player1_id
+                ) REFERENCES players
+                (
+                    id
+                ),
+                FOREIGN KEY
+                (
+                    player2_id
+                ) REFERENCES players
+                (
+                    id
+                )""",
+    },
+    "pending_reps": {
+        "pairing_id": "INTEGER",
+        "reporter_id": "INTEGER",
+        "result": "TEXT",
+        "game_number": "INTEGER",
+        "timestamp": "DATETIME DEFAULT CURRENT_TIMESTAMP",
+        "foreignkeyconstraint": """
+                FOREIGN KEY
+                (
+                    pairing_id
+                ) REFERENCES pairings
+                (
+                    id
+                )
+                FOREIGN KEY
+                (
+                    reporter_id
+                ) REFERENCES players
+                (
+                    id
+                )
+                """,
+    },
+    "seasons": {"active": "INTEGER DEFAULT 0"},
+    "match_history": {
+        "whiteplayer": "INTEGER",
+        "blackplayer": "INTEGER",
+        "colorwon": "TEXT",
+        "season": "TEXT",
+        "league": "TEXT",
+        "foreignkeyconstraint": """
+                FOREIGN KEY
+                (
+                    whiteplayer
+                ) REFERENCES players
+                (
+                    id
+                ),
+                FOREIGN KEY
+                (
+                    blackplayer
+                ) REFERENCES players
+                (
+                    id
+                )""",
+    },
+    "elo_history": {
+        "player_id": "INTEGER",
+        "elo_change": "INTEGER",
+        "timestamp": "DATETIME DEFAULT CURRENT_TIMESTAMP",
+        "foreignkeyconstraint": """
+                FOREIGN KEY
+                (
+                    player_id
+                ) REFERENCES players
+                (
+                    id
+                )""",
+    },
+    "player_aliases": {
+        "player_id": "INTEGER",
+        "alias": "STRING UNIQUE",
+        "foreignkeyconstraint": """
+                FOREIGN KEY
+                (
+                    player_id
+                ) REFERENCES players
+                (
+                    id
+                )""",
+    },
 }
